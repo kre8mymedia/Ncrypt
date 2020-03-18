fca3362ef492bff71b4ffe169ad2df2693b021e9
unknown <kre8mymedia@gmail.com>
2020-03-17 23:49:10 -0500

    first commit

---
+++ upload.php
@@ -0,0 +1,35 @@
+<?php
+$dir = "./files/upload";
+
+// List files inside dir
+$things = scandir($dir);
+// Remove unecessaries
+array_shift($things);
+array_shift($things);
+
+$files = [];
+
+foreach($things as $thing) {
+  $file = str_replace(" ", "_", $thing);
+  array_push($files, $file);
+}
+print_r($files);
+// die();
+
+$file_links = [];
+
+if (count($files) > 0) {
+
+  foreach ($files as $file) { 
+
+    $command = shell_exec('curl -X POST "https://siasky.net/skynet/skyfile" -F file=@files/upload/' . $file);
+    echo shell_exec('rm -rf files/upload/' . $file);
+    $json = json_decode($command, true);
+    $skylink = $json['skylink'];
+    $url = 'https://siasky.net/' . $skylink;
+    // echo $url;
+    array_push($file_links, $url);
+  }
+}
+
+print_r($file_links);
