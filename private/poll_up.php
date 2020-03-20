2672ca8b66f7bf02b00785473b77339bddc9f86f
unknown <kre8mymedia@gmail.com>
2020-03-19 21:20:36 -0500

    added initial Index_Ncrypt to copy.address_book.json

---
+++ private/poll_up.php
@@ -0,0 +1,7 @@
+<?php
+$y = -1;
+
+while ($y < 0) { 
+  echo shell_exec('php upload.php');
+  sleep(2);
+}
--- private/upload.php
+++ private/upload.php
@@ -1,7 +1,6 @@
 <?php
 
 $t = time();
-// $repo = $t . "_" . $argv[1];
 $dir = "./files/upload";
 
 // List files inside dir
@@ -10,32 +9,56 @@ $things = scandir($dir);
 array_shift($things);
 array_shift($things);
 
+// Generate range AlphaNumeric string with length of N
+function generateRandomString($length = 64) {
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
+}
+
+// Generate Random 64bit characterString
+$encryption_key = generateRandomString();
+$ciphering = "AES-128-CTR";
+$iv_leng = openssl_cipher_iv_length($ciphering);
+$options = 0;
+$encryption_iv = '1234567891011121';
+
+// Declare empty array to reference
 $files = [];
 
+// Foreach file in directory
 foreach($things as $thing) {
   $file = str_replace(" ", "_", $thing);
   array_push($files, $file);
 }
-// print_r($files);
-// die();
 
+// Declare empty links array to reference
 $file_links = [];
 
+// If the files array count is less than 0...
 if (count($files) > 0) {
 
-  foreach ($files as $file) { 
+  // For each file in files array...
+  foreach ($files as $file) {
 
+    // Get file type from extension
     $ext = pathinfo($file);
     $file_type = $ext['extension'];
 
+    // Upload using shell exec with the filename on the end
     $command = shell_exec('curl -X POST "https://siasky.net/skynet/skyfile" -F file=@files/upload/' . $file);
-    
+
+    // After uploaded remove from upload folder
     echo shell_exec('rm -rf files/upload/' . $file);
     $json = json_decode($command, true);
     $skylink = $json['skylink'];
     $url = 'https://siasky.net/' . $skylink;
-    // echo $url;
     array_push($file_links, $url);
+
   }
 }
 
