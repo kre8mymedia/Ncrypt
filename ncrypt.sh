c734bce8f4ee6e18cf5fbdd30612d224f59a866d
unknown <kre8mymedia@gmail.com>
2020-03-22 22:18:35 -0500

    configged launch for linux

--- ncrypt.sh
+++ ncrypt.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "Open VScode to Ncrypt Directory"
 
+# If you need php
+#sudo apt-get install php7.3
+
+echo "Opening VScode to Ncrypt Directory"
+
+# If on Ubuntu 18.04
+#cd /var/www/ncrypt
+
+# Specify this on windows using Git Bash
 cd /c/xampp/htdocs/ncrypt
 
 code .
